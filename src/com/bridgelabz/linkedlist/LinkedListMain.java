1b5f360cab4445905a9b51363468826a6258ef02
VighneshBilgi <vhbilgi@gmail.com>
2022-08-08 20:10:42 +0530

    Added: Welcome to the Linked List Program

---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListMain {
+
+    public static void main(String args[]){
+
+        System.out.println("Welcome to Linked List Program");
+
+    }
+
+
+}
